1fee3e8ec4a90ce37a9dc63142942dc454029557
HedleyBenaiges <hedleybenaiges@gmail.com>
2026-03-20 12:02:39 +0000

    first commit

---
+++ bootloader_story.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Ignore Ctrl+C (SIGINT)
+trap '' INT
+# Stop echoing keystrokes
+stty -echo
+# Hide cursor
+tput civis
+
+# Ensure that echo is restored even if crash
+trap 'stty echo; tput cnorm; exit' EXIT
+
+MESSAGE_DIRECTORY="/usr/local/share/stories/"
+message_file = ls ~/.config/scripts/bootloader_story/stories | sort -R | tail -$N
+MESSAGE = "${MESSAGE_DIRECTORY}/${message_file}"
+
+# Clear screen for a clean look
+# echo -e "\e[H\e[2J" 
+
+# Sleep to allow it to start at the start
+# Pretty shoddy programming but shush
+sleep 10
+
+if [ -f "$MESSAGE_FILE" ]; then
+    while IFS= read -r line; do
+        # Print line in bright green (Terminal style)
+        echo -e "\e[32m$line\e[0m"
+
+        # We pick a number between 2 and 30 (tenths of a second)
+        # Then divide by 10 to get 0.2 to 3.0
+        # PAUSE=$(awk -v min=0.2 -v max=3.0 'BEGIN{srand(); print min+rand()*(max-min)}')
+        # sleep "$PAUSE"
+	sleep 3
+    done < "$MESSAGE_FILE"
+fi
+
+sleep 3
+
+# Explicitly restore terminal settings
+stty echo
+tput cnorm
